a11a8d748101f82c32fa3e4a7c761b15db02cbeb
Tina <fatina.kalyanovna@gmail.com>
2023-12-25 22:09:19 +0300

    added Behavior

---
+++ HW_2/ActorBehavior.java
@@ -0,0 +1,10 @@
+public class ActorBehavior {
+    void setMakeOrder(boolean status);
+
+    void setTakeOrder(boolean status);
+
+    boolean isMakeOrder(); // сделап заказ
+
+    boolean isTakeOrder(); // получил заказ
+
+}
