a83670321576142f8e28ea4fe3acc255c4e021ba
Tina <fatina.kalyanovna@gmail.com>
2023-12-25 22:30:39 +0300

    added 2d

--- HW_2/hw_2.java
+++ HW_2/hw_2.java
@@ -2,8 +2,8 @@ public class hw_2 {
     public static void main(String[] args) {
 
         Market market = new Market();
-        Human human1 = new Human("Дима");
-        Human human2 = new Human("Света");
+        Human human1 = new Human("Толик");
+        Human human2 = new Human("Вова");
 
         market.acceptToMarket(human2);
         market.acceptToMarket(human1);
