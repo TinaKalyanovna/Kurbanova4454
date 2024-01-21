7872eb56d3ab67aa5db1c1859ccdd830c6e7477d
Tina <fatina.kalyanovna@gmail.com>
2024-01-21 21:51:56 +0300

    added userController

---
+++ HW_4/controller/UserController.java
@@ -0,0 +1,14 @@
+
+import OOP.Seminar_4.data.User;
+
+import java.time.LocalDate;
+
+
+public class UserController implements IUserController{
+    public void printConsole(User user){}
+
+    @Override
+    public void create(String firstName, String secondName) {
+
+    }
+}
