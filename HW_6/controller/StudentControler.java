0dc094476ff9164930379307dccb0b643dfbc97c
Tina <fatina.kalyanovna@gmail.com>
2024-02-01 21:39:41 +0300

    added studentController

---
+++ HW_6/controller/StudentControler.java
@@ -0,0 +1,24 @@
+package controller;
+import model.Student;
+import service.StudentService;
+import view.StudentView;
+
+
+public class StudentControler {
+    private final StudentService studentService = new StudentService();
+    private final StudentView studentView = new StudentView();
+
+
+    @Override
+    public void create(String firstName, String secondName, String lastName) {
+        studentService.create(firstName, secondName, lastName);
+
+    }
+
+    @Override
+    public void sendOnConsole() {
+        studentView.sendOnConsole(studentService.getStudentList());
+
+    }
+
+}
