0a5df0cdc441816a8772630bbc56a1186d6a6f04
Tina <fatina.kalyanovna@gmail.com>
2024-02-01 21:13:23 +0300

    added groupservice

---
+++ HW_5/service/GroupService.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+import model.Group;
+import model.User;
+
+
+public class GroupService {
+    public Group createGroup (int number, User teacher, List<User> students) {
+
+
+        return new Group(number,teacher,students);
+    }
+
+}
