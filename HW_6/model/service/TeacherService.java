f993248c47951169ace131bbc459f7579886a651
Tina <fatina.kalyanovna@gmail.com>
2024-02-06 14:09:09 +0300

    added teacherService

---
+++ HW_6/model/service/TeacherService.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Teacher;
+
+
+public class TeacherService {
+    private final List<Teacher> teachersList = new ArrayList<>();
+
+    public TeacherService() {
+    }
+
+    @Override
+    public void create(String firstName, String secondName, String lastName) {
+        int maxID = 0;
+        for (Teacher teacher : teachersList) {
+            if (maxID < teacher.getTeacherID()) {
+                maxID = teacher.getTeacherID();
+            }
+
+        }
+        teachersList.add(new Teacher(++maxID,firstName, secondName, lastName));
+
+    }
+    public List<Teacher> getTeachersList() {
+        return teachersList;
+    }
+
+}
